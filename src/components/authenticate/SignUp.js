3bf33e18e8c8ae652428f943dd73a7a53bc76dce
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-05-18 15:41:04 -0400

    add signupLoading props to state in signup component

--- src/components/authenticate/SignUp.js
+++ src/components/authenticate/SignUp.js
@@ -1,134 +1,194 @@
-import React from 'react';
-import { Input, Alert } from 'reactstrap';
-import { connect } from 'react-redux';
-import { signup } from './../../actions/usersActions';
-import { withRouter, Link } from 'react-router-dom';
+import React from "react";
+import { Input, Alert } from "reactstrap";
+import { connect } from "react-redux";
+import { signup } from "./../../actions/usersActions";
+import { withRouter, Link } from "react-router-dom";
+import loading from "./../../../src/loading.gif";
 
 class SignUp extends React.Component {
-   constructor(props) {
-        super(props); 
-        this.state = {
-            username: '',
-            password: '',
-            passRepeat: '',
-            usernameError: false,
-            passwordError: false,
-            passRepeatError: false,
-            notMatchingPassError: false
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      passRepeat: "",
+      usernameError: false,
+      passwordError: false,
+      passRepeatError: false,
+      notMatchingPassError: false,
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      username: "",
+      password: "",
+      passRepeat: "",
+      usernameError: false,
+      passwordError: false,
+      passRepeatError: false,
+      notMatchingPassError: false,
+    });
+  }
+  changeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-   }
-    componentDidMount() {
+  signupHandler = e => {
+    e.preventDefault();
+    if (
+      this.state.username.length === 0 &&
+      this.state.password.length === 0 &&
+      this.state.passRepeat.length === 0
+    ) {
+      this.setState({
+        usernameError: true,
+        passwordError: true,
+        passRepeatError: true,
+      });
+    } else if (
+      this.state.username.length !== 0 &&
+      this.state.password.length === 0 &&
+      this.state.passRepeat.length === 0
+    ) {
+      this.setState({
+        usernameError: false,
+        passwordError: true,
+        passRepeatError: true,
+      });
+    } else if (
+      this.state.username.length === 0 &&
+      this.state.password.length !== 0 &&
+      this.state.passRepeat.length === 0
+    ) {
+      this.setState({
+        usernameError: true,
+        passwordError: false,
+        passRepeatError: true,
+      });
+    } else if (
+      this.state.username.length !== 0 &&
+      this.state.password.length !== 0 &&
+      this.state.passRepeat.length === 0
+    ) {
+      this.setState({
+        usernameError: false,
+        passwordError: false,
+        passRepeatError: true,
+      });
+    } else if (
+      this.state.username.length === 0 &&
+      this.state.password.length === 0 &&
+      this.state.passRepeat.length !== 0
+    ) {
+      this.setState({
+        usernameError: true,
+        passwordError: true,
+        passRepeatError: false,
+      });
+    } else {
+      if (this.state.password !== this.state.passRepeat) {
         this.setState({
-            username: '',
-            password: '',
-            passRepeat: '',
-            usernameError: false,
-            passwordError: false,
-            passRepeatError: false,
-            notMatchingPassError: false
-        })
-    }
-    changeHandler = e => {
+          usernameError: false,
+          passwordError: false,
+          passRepeatError: false,
+          notMatchingPassError: true,
+        });
+      } else {
         this.setState({
-            [e.target.name]: e.target.value,
-        })
-        
+          usernameError: false,
+          passwordError: false,
+          passRepeatError: false,
+          notMatchingPassError: false,
+        });
+        this.props.signup(
+          this.state.username,
+          this.state.password,
+          this.props.history
+        );
+      }
     }
+  };
 
-    signupHandler = e => {
-        e.preventDefault();
-        if (this.state.username.length === 0 && this.state.password.length === 0 && this.state.passRepeat.length === 0) {
-            
-            this.setState({usernameError: true, passwordError: true, passRepeatError: true})
+  render() {
+    return (
+      <div className="login">
+        {this.props.signupLoading ? (
+          <img
+            src={loading}
+            className="loading"
+            alt="PostForm form is loading gif"
+          />
+        ) : (
+          <>
+            <form className="form login-form" onSubmit={this.signupHandler}>
+              <h1>Sign Up</h1>
+              <Input
+                onChange={this.changeHandler}
+                placeholder="Username"
+                name="username"
+                type="text"
+                value={this.state.username}
+              />
+              {this.state.usernameError ? (
+                <Alert color="danger">Username is required.</Alert>
+              ) : null}
 
-        } else if (this.state.username.length !== 0 && this.state.password.length === 0 && this.state.passRepeat.length === 0) {
-            
-            this.setState({usernameError: false, passwordError: true, passRepeatError: true })
+              <Input
+                onChange={this.changeHandler}
+                placeholder="Password"
+                name="password"
+                type="password"
+                value={this.state.password}
+              />
+              {this.state.passwordError ? (
+                <Alert color="danger">Password is required.</Alert>
+              ) : null}
 
-        } else if (this.state.username.length === 0 && this.state.password.length !== 0 && this.state.passRepeat.length === 0) {
-            
-            this.setState({usernameError: true, passwordError: false, passRepeatError: true})
+              <Input
+                onChange={this.changeHandler}
+                placeholder="Confirm Password"
+                name="passRepeat"
+                type="password"
+                value={this.state.passRepeat}
+              />
+              {this.state.passRepeatError ? (
+                <Alert color="danger">Password repeat is required.</Alert>
+              ) : null}
 
-        } else if (this.state.username.length !== 0 && this.state.password.length !== 0 && this.state.passRepeat.length === 0) {
-            
-            this.setState({usernameError: false, passwordError: false, passRepeatError: true})
+              {this.state.notMatchingPassError === true ? (
+                <Alert color="danger">Passwords do not match.</Alert>
+              ) : null}
 
-        } else if (this.state.username.length === 0 && this.state.password.length === 0 && this.state.passRepeat.length !== 0) {
-            
-            this.setState({usernameError: true, passwordError: true, passRepeatError: false})
-
-        } else {
-
-            if (this.state.password !== this.state.passRepeat) {
-                
-                this.setState({usernameError: false, passwordError: false, passRepeatError: false, notMatchingPassError: true})
-
-            } else {
-                
-                this.setState({usernameError: false, passwordError: false, passRepeatError: false, notMatchingPassError: false})
-                this.props.signup(this.state.username, this.state.password, this.props.history);
-
-            }
-        }
-
-    }
-
-    render() {
-
-        return(
-            <div className="login">
-                <form className="form login-form" onSubmit={this.signupHandler}>
-                    <h1>Sign Up</h1>
-                    <Input
-                        onChange={this.changeHandler}
-                        placeholder="Username"
-                        name="username"
-                        type="text"
-                        value={this.state.username}
-                    />
-                    { this.state.usernameError ? <Alert color="danger">Username is required.</Alert> : null }
-                    
-                    <Input
-                        onChange={this.changeHandler}
-                        placeholder="Password"
-                        name="password"
-                        type="password"
-                        value={this.state.password}
-                    />
-                    { this.state.passwordError ? <Alert color="danger">Password is required.</Alert> : null }
-                    
-                    <Input
-                        onChange={this.changeHandler}
-                        placeholder="Confirm Password"
-                        name="passRepeat"
-                        type="password"
-                        value={this.state.passRepeat}
-                    />
-                    { this.state.passRepeatError ? <Alert color="danger">Password repeat is required.</Alert> : null }
-                    
-                    { this.state.notMatchingPassError === true ? <Alert color="danger">Passwords do not match.</Alert> : null }
-
-                    <button className="btn btn-info" type="submit">Sign Up</button>
-                </form>
-                <div className="sign-up">
-                    <h4>
-                        Already have an account? 
-                        <Link to={'/login'}>
-                            <button className="btn btn-success">Log In</button>
-                        </Link>
-                    </h4>
-                    
-                </div>
-
+              <button className="btn btn-info" type="submit">
+                Sign Up
+              </button>
+            </form>
+            <div className="sign-up">
+              <h4>
+                Already have an account?
+                <Link to={"/login"}>
+                  <button className="btn btn-success">Log In</button>
+                </Link>
+              </h4>
             </div>
-        )
-    }
+          </>
+        )}
+      </div>
+    );
+  }
 }
 const MapStateToProps = ({ usersReducer: state }) => {
-    return {
-        username: state.user.username,
-        password: state.user.password
-    }
-}
-export default withRouter(connect(MapStateToProps, {signup})(SignUp));
+  return {
+    username: state.user.username,
+    password: state.user.password,
+    signupLoading: state.signupLoading,
+  };
+};
+export default withRouter(
+  connect(
+    MapStateToProps,
+    { signup }
+  )(SignUp)
+);
