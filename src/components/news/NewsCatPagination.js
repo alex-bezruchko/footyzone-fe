1588c3ae084edbad6fc539a6189242ad9103e2bc
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-07-27 20:24:44 -0400

    change loadig gif, add separated pagination files

---
+++ src/components/news/NewsCatPagination.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Pagination = ({
+  newsPerPage,
+  totalNews,
+  paginate,
+  subcat_name,
+  currentPage,
+}) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalNews / newsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  let link = document.getElementById(`page-${currentPage}`);
+  if (link) {
+    let links = Array.from(document.getElementsByClassName("page-link"));
+    if (links) {
+      links.map(link => (link.className = "page-link"));
+    }
+    link.className = "page-link active-page";
+  }
+  return (
+    <div className="col-md-8">
+      <div className="pagination">
+        {pageNumbers.map(number => (
+          <li key={number} className="page-item">
+            <Link
+              onClick={() => paginate(number)}
+              to={`/news/${subcat_name}/page/${number}`}
+              className="page-link"
+              id={`page-${number}`}
+            >
+              {number}
+            </Link>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
---
+++ src/components/news/NewsPagination.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import $ from "jquery";
+
+const NewsPagination = ({
+  newsPerPage,
+  totalNews,
+  paginate,
+  props,
+  subcat_name,
+  currentPage,
+}) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalNews / newsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+  let link = document.getElementById(`page-${props.match.params.page_id}`);
+  $.when(link).then(function() {
+    $(`#page-${props.match.params.page_id} a`).addClass("active-page");
+  });
+  return (
+    <div className="col-md-8">
+      <div className="pagination">
+        {pageNumbers.map(number => (
+          <li key={number} id={`page-${number}`} className="page-item">
+            <Link
+              onClick={() => paginate(number)}
+              to={`/news/${number}`}
+              className="page-link"
+              id={`page-${number}`}
+            >
+              {number}
+            </Link>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NewsPagination;
