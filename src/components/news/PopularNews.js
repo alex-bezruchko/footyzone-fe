3a65e836e3e3aff505e0e95906f4569f919b26f5
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-07-29 18:44:56 -0400

    add popular news tab

---
+++ src/components/news/PopularNews.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { connect } from "react-redux";
+import $ from "jquery";
+import { withRouter } from "react-router-dom";
+import loading from "./../../../src/loading.gif";
+import { Link } from "react-router-dom";
+import { fetchPopular } from "../../actions/newsActions";
+
+class PopularNews extends React.Component {
+  componentDidMount() {
+    this.props.fetchPopular();
+    console.log(this.props.news);
+  }
+
+  render() {
+    return (
+      <>
+        {this.props.loading ? (
+          <div className="sidebar-list">
+            {/* <img alt="Loading gif" src={loading} /> */}
+          </div>
+        ) : (
+          <div className="sidebar-list">
+            <h2>Trending</h2>
+            {this.props.news.length > 0 ? (
+              <>
+                {this.props.news.map((news, index) => {
+                  return (
+                    <Link
+                      key={index}
+                      to={`/news/${news.subcat_slug}/${news.id}`}
+                    >
+                      <div
+                        id={news.id}
+                        news={news}
+                        className="sidebar-list-post"
+                      >
+                        <img src={news.newsMainImg} alt="" />
+                        <p>{news.title}</p>
+                      </div>
+                    </Link>
+                  );
+                })}
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
+        )}
+      </>
+    );
+  }
+}
+const mapStateToProps = ({ newsReducer: state }) => {
+  return {
+    news: state.news,
+    loading: state.loading,
+  };
+};
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    {
+      fetchPopular,
+    }
+  )(PopularNews)
+);
