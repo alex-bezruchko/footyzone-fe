a1dd039c7a6a8a9c557777912c5c1aa81509dcb1
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-07-29 23:38:30 -0400

    add post loading restrictions

--- src/components/blog/SingleBlog.js
+++ src/components/blog/SingleBlog.js
@@ -42,7 +42,7 @@ class SingleView extends React.Component {
           </div>
         ) : (
           <div className="container single-blog-wrapper">
-            {this.props.post && (
+            {this.props.post && this.props.post.title ?(
               <div className="single col-md-8">
                 {" "}
                 <h2>{this.props.post.title}</h2>
@@ -54,7 +54,8 @@ class SingleView extends React.Component {
                 <div className="single-info">
                   <div className="single-share">
                     <div className="socials">
-                      {this.props.post && this.props.post.title.length > 0 ? (
+                      {this.props.post.title &&
+                      this.props.post.title.length > 0 ? (
                         <>
                           <FacebookShareButton
                             url={window.location.href}
