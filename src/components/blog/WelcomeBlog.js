ce9590e39ae7e64e7048b12db4c887da2027e9f9
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-08-26 12:36:23 -0400

    add css restrictions

--- src/components/blog/WelcomeBlog.js
+++ src/components/blog/WelcomeBlog.js
@@ -15,7 +15,9 @@ class WelcomeBloglist extends React.Component {
         this.props.fetchLatestPosts();
         let container = document.getElementsByClassName('container-row new-blog');
         $.when(container[0]).then(function () {
-            container[0].style.background = `url(${stadium}) no-repeat cover center center`;
+            if (container[0].clientWidth < 500) {
+                container[0].style.background = `url(${stadium}) no-repeat cover center center`;
+            }
             console.log(container[0])
         })
     }
