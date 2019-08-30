358b8916b6811592a4fd66b8327c3b956f3b8b4f
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-08-30 10:05:00 -0400

    add share links to blog list

--- src/components/blog/Postslist.js
+++ src/components/blog/Postslist.js
@@ -1,7 +1,13 @@
 import loadingImage from "./../../../src/loading.gif";
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
 
+} from "react-share";
 const PostsList = ({ currentPage, posts, loading, props }) => {
   if (loading) {
     return (
@@ -23,9 +29,31 @@ const PostsList = ({ currentPage, posts, loading, props }) => {
           <Link to={`/blog/${post.id}`}>
             <h2>{post.title}</h2>
             <img className="col-md-8-img" src={post.postMainImg} alt="" />
+            <div className="blog-share">
+              <FacebookShareButton
+                url={window.location.href}
+                media={post.title}
+                className="button"
+              >
+                <FacebookIcon size={32} round={false} />
+              </FacebookShareButton>
+              <TwitterShareButton
+                url={window.location.href}
+                media={post.title}
+                className="button"
+              >
+                <TwitterIcon size={32} round={false} />
+              </TwitterShareButton>
+            </div>
             <div className="body">
               {post.body && post.body.length ?
-                <p>{post.body.slice(0, maxLenBody).concat('...')}</p>
+                <>
+                  <p>
+
+                    {post.body.slice(0, maxLenBody).concat('...')}
+                  </p>
+
+                </>
                 :
                 <p>
                   Kroenke's statement read: "While we understand, appreciate, and agree with concerns about our club failing to achieve our goal of qualifying for the 2019-20 Champions League, we respectfully disagree it is at a crossroads and things need to change because so much change has already occurred.</p>
