7bd1830d242920870170e7fcba0ac8b8098e5a86
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-09-02 08:46:58 -0400

    add fixed bg to old-school scrollable module

--- src/components/oldschool/OldSchoolList.js
+++ src/components/oldschool/OldSchoolList.js
@@ -1,10 +1,10 @@
 import React from "react";
 import axios from "axios";
 import imgLoading from "./../../../src/loading.gif";
+import bricks from "./../../img/blog.jpg";
+
 import $ from "jquery";
 import _ from "lodash";
-
-import { Throttle } from "react-throttle";
 import {
   FaRegNewspaper,
 } from "react-icons/fa";
@@ -18,7 +18,7 @@ class OldSchoolList extends React.Component {
       cart: [],
       initLoading: false,
       moreLoading: false,
-      counter: 0,
+      counter: 1,
       current_height: 200
     }
     this.fetchMore = this.fetchMore.bind(this);
@@ -33,18 +33,19 @@ class OldSchoolList extends React.Component {
         if (response.data) {
           let newCart = [];
           newCart.push(response.data[0]);
+          newCart.push(response.data[1]);
           this.setState({
             news: response.data,
             initLoading: false,
             cart: newCart,
-            counter: 1
+            counter: 2
           });
         } else {
           this.setState({
             news: [],
             initLoading: false,
             cart: [],
-            counter: 0
+            counter: 1
           });
         }
       })
@@ -53,9 +54,48 @@ class OldSchoolList extends React.Component {
           news: [],
           initLoading: false,
           cart: [],
-          counter: 0
+          counter: 1
         });
       });
+
+    // OLD SCHOOL MOBILE FIXED SCROLLABLE BACKGROUND
+    const old_school = document.getElementsByClassName('container-row welcome-blog')
+    const position = $(".container-row.welcome-blog").position();
+    // const old_school_container = document.getElementById('old-school-list');
+    // const carousel = $(".container.carousel-container");
+    // console.log(link_height)
+    // let 
+    const link_height = $("#old-school-list a").height();
+    $.when(old_school[0]).then(function () {
+      // let old_school = this.state.news;
+      old_school[0].style.background = `url(${bricks})`;
+      old_school[0].style.backgroundRepeat = "repeat";
+      old_school[0].style.backgroundSize = "container";
+
+
+      // let old_school_list_height = link_height * old_school.length;
+      // console.log(this.state.news.length)
+      // console.log(old_school_list_height)
+      if (old_school[0] && old_school[0].clientWidth < 500) {
+        $(window).scroll(function () {
+          // if (this.state.news && this.state.news.length > 0) {
+          if (($(window).scrollTop() > 50) && (($(window).scrollTop() < old_school[0].clientHeight - 500))) {
+            $('.oldschool-bg').css({ "position": "fixed", "top": "0" });
+          } else if (($(window).scrollTop() > old_school[0].clientHeight + 100)) {
+            $('.oldschool-bg').css({ "position": "absolute", "top": "" });
+
+          }
+          else {
+            $('.oldschool-bg').css({ "position": "absolute", "top": "" });
+          }
+          // } 
+
+        });
+      }
+    }).catch(function (err) {
+      console.log(err)
+      this.props.history.push(`/${this.props.location.pathname}`)
+    })
   }
 
   componentDidMount() {
@@ -96,12 +136,12 @@ class OldSchoolList extends React.Component {
       }
 
     });
-    $(window).on("scroll", _.throttle(this.fetchMore, 1750))
+    $(window).on("scroll", _.throttle(this.fetchMore, 2000))
 
 
   }
 
-  fetchMore() {
+  fetchMore(e) {
     // console.log(h)
     let scrollTop = $(window).scrollTop()
     let currentCount = this.state.counter;
@@ -113,8 +153,7 @@ class OldSchoolList extends React.Component {
       currentCart = this.state.cart;
       currentNews = this.state.news
     }
-    console.log(scrollTop)
-    console.log(this.state.counter)
+
     if (height < scrollTop) {
       this.setState({
         moreLoading: true,
@@ -136,22 +175,21 @@ class OldSchoolList extends React.Component {
         let h2 = document.createElement("H2");
         let img = document.createElement("IMG");
         let div = document.createElement("DIV");
+        div.className = "body";
 
 
         let list = document.getElementsByClassName('category-news');
 
         if (list[0] && link) {
-          console.log(list[0])
-          console.log(link)
 
           if (currentNews && currentNews.length > 0 && (currentNews.length - 1 > this.state.counter)) {
-            link.href = `/news/${currentNews[this.state.counter].subcat_slug}/${currentNews[this.state.counter].id}`;
+            link.href = `/old-school/${currentNews[this.state.counter].subcat_slug}/${currentNews[this.state.counter].id}`;
+            // link.href = `/old-school/${currentNews[this.state.counter].id}`;
             h2.innerText = currentNews[this.state.counter].title;
             img.src = currentNews[this.state.counter].newsMainImg;
             div.innerText = currentNews[this.state.counter].body.slice(0, 250).concat('...');
-            div.className = "body";
-            link.appendChild(h2)
             link.appendChild(img)
+            link.appendChild(h2)
             link.appendChild(div)
             list[0].appendChild(link)
             currentCart.push(currentNews[this.state.counter])
@@ -164,6 +202,14 @@ class OldSchoolList extends React.Component {
           };
         }
       }
+      e.preventDefault()
+      e.stopPropagation();
+
+    } else {
+      e.preventDefault()
+      e.stopPropagation();
+
+
     }
   }
 
@@ -177,9 +223,11 @@ class OldSchoolList extends React.Component {
     console.log(this.state.cart)
     let maxLenBody = 250;
     return (
-      <div className="container-row news">
+      <div className="container-row welcome-blog">
+        <img src={bricks} alt="shiny photoshopped stadium" className="oldschool-bg" />
+
         {this.state.initLoading ? (
-          <div className="container blog-list">
+          <div className="container">
             <div className="col-md-8">
               <img className="loading" alt="Loading gif" src={imgLoading} />
             </div>
@@ -187,26 +235,36 @@ class OldSchoolList extends React.Component {
         ) : (
             // <Throttle handler="onScroll">
             <div onScroll={this.fetchMore} className="container-row">
-              <div className="news-list container">
-                <h1 className="category-header">Latest <FaRegNewspaper /> </h1>
-                <div className="col-sm-12 col-md-8">
+              <div className="blog-list container">
+                <h1 className="category-header">Old School <FaRegNewspaper /> </h1>
+                <div id="old-school-list" className="col-sm-8">
                   {this.state.news.length > 0 ? (
                     <div className="list-wrapper">
                       <div className="category-news">
 
-                        <Link to={`/news/${this.state.news[0].subcat_slug}/${this.state.news[0].id}`}>
-                          <h2>{this.state.news[0].title}</h2>
+                        {/* <Link to={`/old-school/${this.state.news[0].id}`}> */}
+                        <Link to={`/old-school/${this.state.news[1].subcat_slug}/${this.state.news[0].id}`}>
                           <img
-                            className="col-md-8-img"
                             src={this.state.news[0].newsMainImg}
                             alt=""
                           />
+                          <h2>{this.state.news[0].title}</h2>
                           <div className="body">{this.state.news[0].body.slice(0, maxLenBody).concat('...')}</div>
                         </Link>
-                        {this.state.moreLoading ? (
+                        {/* <Link to={`/old-school/${this.state.news[1].id}`}> */}
+
+                        <Link to={`/old-school/${this.state.news[1].subcat_slug}/${this.state.news[1].id}`}>
+                          <img
+                            src={this.state.news[1].newsMainImg}
+                            alt=""
+                          />
+                          <h2>{this.state.news[1].title}</h2>
+                          <div className="body">{this.state.news[1].body.slice(0, maxLenBody).concat('...')}</div>
+                        </Link>
+                        {!this.state.moreLoading ? (
                           <div className="container blog-list">
-                            <div className="col-md-8">
-                              <img className="loading" alt="Loading gif" src={imgLoading} />
+                            <div className="more-blog">
+                              <img className="loading more-loading" alt="Loading gif" src={imgLoading} />
                             </div>
                           </div>
                         ) : (<></>)}
