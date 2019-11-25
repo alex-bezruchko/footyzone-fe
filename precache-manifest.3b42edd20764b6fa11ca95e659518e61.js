6a6cf6e9c87872e1db8ff3791b15987fe11cdaac
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-11-24 20:00:09 -0500

    Updates

---
+++ precache-manifest.3b42edd20764b6fa11ca95e659518e61.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "93c9cfbe0ea679f9c73b",
+    "url": "/footyzone-fe/static/css/main.53871dda.chunk.css"
+  },
+  {
+    "revision": "93c9cfbe0ea679f9c73b",
+    "url": "/footyzone-fe/static/js/main.efc63e47.chunk.js"
+  },
+  {
+    "revision": "889cba41fa151066603d",
+    "url": "/footyzone-fe/static/js/runtime~main.e7e1a637.js"
+  },
+  {
+    "revision": "7ec9366504910d4b1371",
+    "url": "/footyzone-fe/static/css/2.25645b3c.chunk.css"
+  },
+  {
+    "revision": "7ec9366504910d4b1371",
+    "url": "/footyzone-fe/static/js/2.bf4c2a40.chunk.js"
+  },
+  {
+    "revision": "ef26001cb39405102a6c32f34f37487e",
+    "url": "/footyzone-fe/static/media/loading.ef26001c.gif"
+  },
+  {
+    "revision": "8cd0533d5cb228adbd06e92356e9ae2a",
+    "url": "/footyzone-fe/static/media/blank.8cd0533d.jpeg"
+  },
+  {
+    "revision": "5323c733ecf02da1dbcedbaa80924edf",
+    "url": "/footyzone-fe/static/media/missing.5323c733.jpg"
+  },
+  {
+    "revision": "3bd9dbb0c3c508d0b545aab2b133f20c",
+    "url": "/footyzone-fe/index.html"
+  }
+];
